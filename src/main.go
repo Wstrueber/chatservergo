3b71a0f5f2626ef7f03aeeb0683455d5340e48bc
Wstrueber <42927698+Wstrueber@users.noreply.github.com>
2020-02-08 18:08:57 +0100

    log

--- src/main.go
+++ src/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"chatservergo/src/app"
+	"fmt"
 )
 
 func main() {
 	app := &app.App{}
 	app.Init()
+	fmt.Println("Server has started on port 8080...")
 	app.Run(":8080")
 }
